db9ec6ca5821f12dfd9ba47616c1b2154241d5bb
Mykhailo <neformal.lviv@gmail.com>
2022-07-21 20:43:19 +0200

    Refactored the New york times page. Add handlebars template

---
+++ the-new-york-times_refactored.js
@@ -0,0 +1,45 @@
+export default {
+  localizations: [
+    { language: "U.S.", url: "https://the-newyork-times/us/" },
+    { language: "International", url: "/" },
+    { language: "Canada", url: "/" },
+    { language: "Español", url: "/" },
+    { language: "中文", url: "/" },
+  ],
+  navigation: [
+    "World",
+    "U.S.",
+    "Politics",
+    "N.Y.",
+    "Business",
+    "Opinion",
+    "Tech",
+    "Science",
+    "Health",
+    "Sports",
+    "Arts",
+    "Books",
+    "Style",
+    "Food",
+    "Travel",
+    "Magazine",
+    "T Magazine",
+    "Real Estate",
+    "Video",
+  ],
+  articles: [
+    {
+      title:
+        "E.U. Nations Are Asked to Immediately Start Rationing Natural Gas",
+      body: "<ul>\n            <li>\n              Warning of possible further disruptions to Russian fuel exports,\n              the European Union is asking members to adopt a strict energy\n              consumption plan. The plan would solidify the sense that\n            </li>\n            <li>\n              Europe’s economy is on a war footing with Russia, and would need\n              the backing of national governments. Follow updates.\n            </li>\n          </ul>",
+    },
+    {
+      title:
+        "President Vladimir Putin signaled that Russian gas would resume in a key pipeline but at a reduced level.",
+    },
+    {
+      title:
+        "In Iran, President Vladimir Putin has found a new ally to help push back against ostracism from the West.",
+    },
+  ],
+};
